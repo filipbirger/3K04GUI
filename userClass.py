8cc24c4c6e389ca2d4fcbf1ea393f1d4ee427391
filipbirger <101757621+filipbirger@users.noreply.github.com>
2023-10-05 15:43:16 -0400

    Update userClass.py

--- userClass.py
+++ userClass.py
@@ -3,8 +3,8 @@
 class userClass:
     def __init__(self, username, password,lowerRateLimit = 0,upperRateLimit = 0, ventricularAmplitude = 0, ventricularPulseWidth = 0, ventricularSensitivity = 0, 
                  VRP = 0, Hysteresis = 0, rateSmoothing = 0, atrialAmplitude = 0, atrialPulseWidth = 0, atrialSensitivity = 0, ARP = 0, PVARP = 0):
-        self.username = username
-        self.password = password
+        self._username = username
+        self._password = password
         self._lowerRateLimit = lowerRateLimit
         self._upperRateLimit = upperRateLimit
         self._atrialAmplitude = atrialAmplitude
@@ -18,18 +18,128 @@ class userClass:
         self._ventricularPulseWidth = ventricularPulseWidth
         self._ventricularSensitivity = ventricularSensitivity
         self._VRP = VRP
-    
-    def getUsername(self):
-        return self.username
-    
-    def getPassword(self):
-        return self.password
-    
-    def setUsername(self, name):
-        self.username = name
 
-    def setPassword(self, p):
-        self.password = p
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        self._username = value
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = value
+
+    @property
+    def lowerRateLimit(self):
+        return self._lowerRateLimit
+
+    @lowerRateLimit.setter
+    def lowerRateLimit(self, value):
+        self._lowerRateLimit = value
+
+    @property
+    def upperRateLimit(self):
+        return self._upperRateLimit
+
+    @upperRateLimit.setter
+    def upperRateLimit(self, value):
+        self._upperRateLimit = value
+
+    @property
+    def atrialAmplitude(self):
+        return self._atrialAmplitude
+
+    @atrialAmplitude.setter
+    def atrialAmplitude(self, value):
+        self._atrialAmplitude = value
+
+    @property
+    def atrialPulseWidth(self):
+        return self._atrialPulseWidth
+
+    @atrialPulseWidth.setter
+    def atrialPulseWidth(self, value):
+        self._atrialPulseWidth = value
+
+    @property
+    def atrialSensitivity(self):
+        return self._atrialSensitivity
+
+    @atrialSensitivity.setter
+    def atrialSensitivity(self, value):
+        self._atrialSensitivity = value
+
+    @property
+    def ARP(self):
+        return self._ARP
+
+    @ARP.setter
+    def ARP(self, value):
+        self._ARP = value
+
+    @property
+    def PVARP(self):
+        return self._PVARP
+
+    @PVARP.setter
+    def PVARP(self, value):
+        self._PVARP = value
+
+    @property
+    def Hysteresis(self):
+        return self._Hysteresis
+
+    @Hysteresis.setter
+    def Hysteresis(self, value):
+        self._Hysteresis = value
+
+    @property
+    def rateSmoothing(self):
+        return self._rateSmoothing
+
+    @rateSmoothing.setter
+    def rateSmoothing(self, value):
+        self._rateSmoothing = value
+
+    @property
+    def ventricularAmplitude(self):
+        return self._ventricularAmplitude
+
+    @ventricularAmplitude.setter
+    def ventricularAmplitude(self, value):
+        self._ventricularAmplitude = value
+
+    @property
+    def ventricularPulseWidth(self):
+        return self._ventricularPulseWidth
+
+    @ventricularPulseWidth.setter
+    def ventricularPulseWidth(self, value):
+        self._ventricularPulseWidth = value
+
+    @property
+    def ventricularSensitivity(self):
+        return self._ventricularSensitivity
+
+    @ventricularSensitivity.setter
+    def ventricularSensitivity(self, value):
+        self._ventricularSensitivity = value
+
+    @property
+    def VRP(self):
+        return self._VRP
+
+    @VRP.setter
+    def VRP(self, value):
+        self._VRP = value
+
+
 
     def AOO(self,lowerRateLimit, upperRateLimit, atrialAmplitude, atrialPulseWidth):
         self.lowerRateLimit = lowerRateLimit
@@ -63,3 +173,19 @@ class userClass:
         self.VRP = VRP
         self.Hysteresis = Hysteresis
         self.rateSmoothing = rateSmoothing
+
+    def delete(self):
+        del self._password 
+        del self._lowerRateLimit 
+        del self._upperRateLimit 
+        del self._atrialAmplitude 
+        del self._atrialPulseWidth 
+        del self._atrialSensitivity 
+        del self._ARP 
+        del self._PVARP 
+        del self._Hysteresis 
+        del self._rateSmoothing 
+        del self._ventricularAmplitude 
+        del self._ventricularPulseWidth
+        del self._ventricularSensitivity 
+        del self._VRP 
