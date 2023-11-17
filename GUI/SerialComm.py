2fecafdeb9d3f5021107aa6084e65bfcef46a902
filipbirger <101757621+filipbirger@users.noreply.github.com>
2023-11-17 09:00:53 -0500

    ser mode

--- GUI/SerialComm.py
+++ GUI/SerialComm.py
@@ -12,20 +12,22 @@ class SerialComm:
             self.comm.baudrate = 115200
             self.comm.port = "Com7"
             self.isConnected = True
+            self.comm.open()
         except serial.SerialException as error:
             print("Error",error)
             self.isConnected = False
 
 
     
-    def serWriteAOO(self, lowerRateLimit, upperRateLimit,atrialAmplitude,atrialPulseWidth):
+    def serWriteAOO(self,mode, lowerRateLimit, upperRateLimit,atrialAmplitude,atrialPulseWidth):
         try:
+            pmode = struct.pack("F",mode)
             pLowerRateLimit = struct.pack("F",lowerRateLimit)
             pUpperRateLimit = struct.pack("F",upperRateLimit)
             pAtrialAmplitude = struct.pack("F",atrialAmplitude )
             pAtrialPulseWidth = struct.pack("F",atrialPulseWidth)
 
-            write = b"\x16\x55"+pLowerRateLimit+pUpperRateLimit+pAtrialAmplitude+pAtrialPulseWidth
+            write = b"\x16\x55"+pmode+pLowerRateLimit+pUpperRateLimit+pAtrialAmplitude+pAtrialPulseWidth
             self.comm.write(write)
         
         except serial.SerialTimeoutException as error:
